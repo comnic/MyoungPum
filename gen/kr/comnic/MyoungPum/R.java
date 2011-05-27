18d52fa208a74fd1c954d4294aa95d6baa25152f
Minho Lee <comnic@hanmail.net>
2011-05-27 22:04:52 +0900

    modified:   AndroidManifest.xml 	modified:   bin/MyoungPum.apk 	modified:   bin/classes.dex 	modified:   bin/kr/comnic/MyoungPum/MyoungPumActivity.class 	modified:   bin/kr/comnic/MyoungPum/R$drawable.class 	modified:   bin/kr/comnic/MyoungPum/R$layout.class 	modified:   bin/kr/comnic/MyoungPum/R$string.class 	modified:   bin/kr/comnic/MyoungPum/R.class 	modified:   bin/resources.ap_ 	modified:   gen/kr/comnic/MyoungPum/R.java 	modified:   res/layout/main.xml 	modified:   res/values/strings.xml 	modified:   src/kr/comnic/MyoungPum/MyoungPumActivity.java 	bin/ 	gen/ 	res/drawable-hdpi/list001.png 	res/drawable-hdpi/list002.png 	res/drawable-hdpi/list003.png 	res/drawable-hdpi/list004.png 	res/drawable-hdpi/logo.png 	res/drawable-hdpi/main1.jpg 	res/drawable-hdpi/main2.jpg 	res/drawable-hdpi/main3.jpg 	res/drawable-hdpi/main4.jpg 	res/layout/ 	res/values/ 	src/

--- gen/kr/comnic/MyoungPum/R.java
+++ gen/kr/comnic/MyoungPum/R.java
@@ -12,9 +12,25 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int list001=0x7f020001;
+        public static final int list002=0x7f020002;
+        public static final int list003=0x7f020003;
+        public static final int list004=0x7f020004;
+        public static final int logo=0x7f020005;
+        public static final int main1=0x7f020006;
+        public static final int main2=0x7f020007;
+        public static final int main3=0x7f020008;
+        public static final int main4=0x7f020009;
+    }
+    public static final class id {
+        public static final int gridList=0x7f050001;
+        public static final int imageView1=0x7f050002;
+        public static final int webkitWebView1=0x7f050000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int detail=0x7f030000;
+        public static final int list=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/kr/comnic/MyoungPum/MyoungPumActivity.java
+++ src/kr/comnic/MyoungPum/MyoungPumActivity.java
@@ -1,13 +1,49 @@
 package kr.comnic.MyoungPum;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MyoungPumActivity extends Activity {
     /** Called when the activity is first created. */
+
+	Intent clsIntent;
+	Timer tt ;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        clsIntent = new Intent( this, ListActivity.class );
+        tt = new Timer();
+        
+        TimerTask tTask = new TimerTask() {
+			@Override
+			public void run() {
+				Log.i("MyoungPumActivity", "run");
+				changeActivety();
+			}
+		};
+		
+		
+		tt.schedule(tTask,1000);
     }
+
+    public void changeActivety(){
+    	startActivity(clsIntent);
+    	finish();
+    }
+
+    @Override
+    protected void onStop() {
+    	tt.cancel();
+    	finish();
+    	super.onStop();
+    	
+    }
+
 }
